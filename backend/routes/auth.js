679aed72761c47aec8de71357541194209888bcb
20120612 <nguyenlamtruong20021@gmail.com>
2023-05-20 00:36:01 +0700

    change

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 
-const authController = require('../controllers/authController');
+const authController = require("../controllers/authController");
 
 module.exports = router;
