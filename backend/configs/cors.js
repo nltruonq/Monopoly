96f87bdaea59784f1aec609b6245f93b0d3fe2ca
20120612 <nguyenlamtruong20021@gmail.com>
2023-06-10 00:07:57 +0700

    fix cors

--- backend/configs/cors.js
+++ backend/configs/cors.js
@@ -1,9 +1,17 @@
-const constants =require("../constant/constant")
+const constants = require("../constant/constant");
 
-const corsConfig={
-    origin: [constants.CLIENT_API, process.env.CLIENT_URL],
-    exposedHeaders: 'Authorization',
+const allowedOrigins = [constants.CLIENT_API, process.env.CLIENT_URL];
+
+const corsConfig = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    exposedHeaders: "Authorization",
     credentials: true,
-}
+};
 
-module.exports=corsConfig
+module.exports = corsConfig;
