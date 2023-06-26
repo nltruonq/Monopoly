3bdb4d17a525417392dccce542da1a2612f56341
20120612 <nguyenlamtruong20021@gmail.com>
2023-06-26 12:07:33 +0700

    make friend

--- backend/controllers/friendRequestController.js
+++ backend/controllers/friendRequestController.js
@@ -1,18 +1,34 @@
-const FriendRequest = require("../models/friendRequest.model")
-const Friend = require("../models/friend.model")
-const User = require("../models/user.model")
+const FriendRequest = require("../models/friendRequest.model");
+const Friend = require("../models/friend.model");
+const User = require("../models/user.model");
 
 const FriendRequestController = {
-
     // Lấy danh sách lời mời kết bạn
     getFriendRequests: async (req, res) => {
         try {
-            const { username } = req.params
-            const friendRequests = await FriendRequest.find({ receiver:username})
+            const { username } = req.params;
+            const friendRequests = await FriendRequest.find({ receiver: username });
 
-            for(let i = 0 ; i<friendRequests.length;i++){
-                const user =  await User.findOne({username: friendRequests[i].sender})
-                friendRequests[i] = {...friendRequests[i]._doc,user}
+            for (let i = 0; i < friendRequests.length; i++) {
+                const user = await User.findOne({ username: friendRequests[i].sender });
+                friendRequests[i] = { ...friendRequests[i]._doc, user };
+            }
+
+            res.status(200).json(friendRequests);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
+
+    // Lấy danh sách lời mời kết bạn đã gửi
+    getFriendRequestsSend: async (req, res) => {
+        try {
+            const { username } = req.params;
+            const friendRequests = await FriendRequest.find({ sender: username });
+
+            for (let i = 0; i < friendRequests.length; i++) {
+                const user = await User.findOne({ username: friendRequests[i].receiver });
+                friendRequests[i] = { ...friendRequests[i]._doc, user };
             }
 
             res.status(200).json(friendRequests);
@@ -24,52 +40,47 @@ const FriendRequestController = {
     // Gửi lời mời kết bạn
     sendFriendRequest: async (req, res) => {
         try {
-            const { sender, receiver } = req.body
-            const friendRequest = new FriendRequest({ sender, receiver })
-            await friendRequest.save()
+            const { sender, receiver } = req.body;
+            const friendRequest = new FriendRequest({ sender, receiver });
+            await friendRequest.save();
             return res.json({
                 status: 200,
-                message: "Gửi thành công"
-            })
+                message: "Gửi thành công",
+            });
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     },
 
     // Chấp nhận lời mời kết bạn
     acceptFriendRequest: async (req, res) => {
         try {
-            const { username_1, username_2 } = req.body
-            const createFriend = new Friend({ username_1, username_2 })
-            await createFriend.save()
-            await FriendRequest.deleteOne({ sender: username_1, receiver: username_2 })
+            const { username_1, username_2 } = req.body;
+            const createFriend = new Friend({ username_1, username_2 });
+            await createFriend.save();
+            await FriendRequest.deleteOne({ sender: username_1, receiver: username_2 });
             return res.json({
                 status: 200,
-                message: "Đã chấp nhận"
-            })
-
+                message: "Đã chấp nhận",
+            });
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     },
 
     // Từ chối lời mời kết bạn
     refuseFriendRequest: async (req, res) => {
         try {
-            const { sender, receiver } = req.body
-            await FriendRequest.deleteOne({ sender, receiver })
+            const { sender, receiver } = req.body;
+            await FriendRequest.deleteOne({ sender, receiver });
             return res.json({
                 status: 200,
-                message: "Đã xóa lời mời"
-            })
-
+                message: "Đã xóa lời mời",
+            });
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    },
+};
 
-
-
-}
-
-module.exports = FriendRequestController
+module.exports = FriendRequestController;
--- backend/routes/friendRequest.js
+++ backend/routes/friendRequest.js
@@ -4,7 +4,8 @@ const FriendRequestController = require("../controllers/friendRequestController"
 
 route.post("/send", FriendRequestController.sendFriendRequest);
 route.post("/accept",FriendRequestController.acceptFriendRequest)
-route.get("/get/:username",FriendRequestController.getFriendRequests)
-route.post("/refuse",FriendRequestController.refuseFriendRequest)
+route.get("/get/:username",FriendRequestController.getFriendRequests);
+route.get("/get-send/:username",FriendRequestController.getFriendRequestsSend);
+route.post("/refuse",FriendRequestController.refuseFriendRequest);
 
 module.exports = route
